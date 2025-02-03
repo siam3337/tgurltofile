fb819f00857d486259a771e7a2b2ac8d6b9d06ab
MD Siam Ehosun Sun <116423968+siam3337@users.noreply.github.com>
2025-02-04 02:14:18 +0600

    Update server.py

--- server.py
+++ server.py
@@ -7,4 +7,5 @@ def home():
     return "Bot is running!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    print("Starting Flask server on port 8000...")  # Debugging
+    app.run(host="0.0.0.0", port=8000, debug=True, use_reloader=False, threaded=True)
