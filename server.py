d8d5809f33e60b165d643efadefc6eb8f14a73c2
MD Siam Ehosun Sun <116423968+siam3337@users.noreply.github.com>
2025-02-04 02:47:06 +0600

    Update server.py

--- server.py
+++ server.py
@@ -8,5 +8,5 @@ def home():
     return "Bot is running!"
 
 if __name__ == "__main__":
-    print("Starting Flask server on port 8000...")  # Debugging
-    app.run(host="0.0.0.0", port=8000, debug=True, use_reloader=False, threaded=True)
+    print("Starting Flask server on port 8000...")  # Debugging (Optional)
+    # Flask's app.run() is removed, Gunicorn will run the app in production
